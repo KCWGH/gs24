59f9f27f80b510a06e797a303935d252c19865ba
moononcat <hanjojangin@naver.com>
2024-12-13 18:06:13 +0900

    대충 잘 함

---
+++ GS24/src/main/java/com/gs24/website/service/ImgFoodServiceImple.java
@@ -0,0 +1,54 @@
+package com.gs24.website.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gs24.website.domain.ImgFoodVO;
+import com.gs24.website.persistence.ImgFoodMapper;
+
+import lombok.extern.log4j.Log4j;
+
+@Service
+@Log4j
+public class ImgFoodServiceImple implements ImgFoodService{
+
+	@Autowired
+	private ImgFoodMapper imgFoodMapper;
+	@Override
+	public int createImgFood(ImgFoodVO imgFoodVO) {
+		log.info("createImgFood() 角青");
+		int result = imgFoodMapper.insertImgFood(imgFoodVO);
+		return result;
+	}
+
+	@Override
+	public ImgFoodVO getImgFoodById(int foodId) {
+		log.info("getImgFoodById");
+		ImgFoodVO imgFoodVO = imgFoodMapper.selectImgFoodById(foodId);
+		return imgFoodVO;
+	}
+
+	@Override
+	public List<ImgFoodVO> getAllImgFood() {
+		log.info("getAllImgFood() 角青");
+		List<ImgFoodVO> list = imgFoodMapper.selectAllImagFood();
+		return list;
+	}
+
+	@Override
+	public int updateImgFood(ImgFoodVO imgFoodVO) {
+		log.info("updateImgFood() 角青");
+		int result = imgFoodMapper.updateImgFood(imgFoodVO);
+		return result;
+	}
+
+	@Override
+	public int deleteImgFood(int foodId) {
+		log.info("deleteImgFood() 角青");
+		int result = imgFoodMapper.deleteImgFood(foodId);
+		return 0;
+	}
+
+}
--- GS24/src/main/webapp/WEB-INF/views/food/list.jsp
+++ GS24/src/main/webapp/WEB-INF/views/food/list.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+<<<<<<< Updated upstream
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html lang="ko">
@@ -18,5 +19,45 @@
 	<a href="../member/login"><button>로그인</button></a>
 	</c:if>
 	<hr>
+=======
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<script src="https://code.jquery.com/jquery-3.7.1.js">
+</script>
+<title>식품 리스트</title>
+<style>
+ul {
+	list-style-type: none;
+}
+
+li {
+	display: inline-block;
+}
+</style>
+
+</head>
+<body>
+	<h1>식품 리스트</h1>
+	<a href=register><p>식품 등록</p></a>
+	<ul class="food_box">
+		<c:forEach var="FoodVO" items="${FoodList }">
+		<!-- 나중에 식품 이미지도 같이 나오게 해야한다. -->
+			<li class='List'>
+				<div class="FoodBox">
+					<p>${FoodVO.foodType }</p>
+					<p>${FoodVO.foodName }</p>
+					<p>${FoodVO.foodStock }개</p>
+					<p>${FoodVO.foodPrice }원</p>
+					<a href = "detail?foodId=${FoodVO.foodId}">자세히 보기</a>
+					<a href="update?foodId=${FoodVO.foodId }"><p>식품 정보 수정</p></a>
+				</div>
+			</li>
+		</c:forEach>
+	</ul>
+>>>>>>> Stashed changes
 </body>
 </html>
