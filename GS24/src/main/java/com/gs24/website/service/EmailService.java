c3285cffb5745e4ffac6cc932accee857431d626
KCWGH <kimzenio07@gmail.com>
2024-12-27 09:45:03 +0900

    최신화 위한 파일 삭제

--- GS24/src/main/java/com/gs24/website/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.gs24.website.service;
-
-import javax.mail.MessagingException;
-
-public interface EmailService {
-	void sendVerificationEmail(String toEmail) throws MessagingException;
-
-	boolean verifyCode(String email, String code);
-}
